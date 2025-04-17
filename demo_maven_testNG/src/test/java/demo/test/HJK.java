e7ec56fcf12301f8a15b695d2b9a98e80efc4476
Sujeeth <sujeeth.poojary@ibm.com>
2025-04-17 14:42:15 +0530

    resolve conflict

--- demo_maven_testNG/src/test/java/demo/test/HJK.java
+++ demo_maven_testNG/src/test/java/demo/test/HJK.java
@@ -3,12 +3,7 @@ package demo.test;
 public class HJK {
 
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
 		System.out.println("Hello World!!");
-=======
-		System.out.println("Hello SMK00989!!");
->>>>>>> branch 'master' of https://github.com/SJGithub1/GitHub-ActionTest-Java-Maven.git
 	}
 
 }
